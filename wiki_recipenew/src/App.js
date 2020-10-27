dd221b7648d55f359707f2e09b34278e86904c31
Nisha <nishashukla1987@gmail.com>
2020-10-27 19:02:17 +0100

    sidenav is fixed

--- wiki_recipenew/src/App.js
+++ wiki_recipenew/src/App.js
@@ -6,6 +6,7 @@ import Recipe from './Components/Pages/Recipe';
 import Navigation from './Components/Navigation/Navigation';
 import { Route, Switch } from 'react-router-dom';
 import SideNav from './Components/SideNav/SideNav';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const SET = (k, v) => localStorage.setItem(k, JSON.stringify(v));
 const GET = (k) => JSON.parse(localStorage.getItem(k));
@@ -23,19 +24,23 @@ function App() {
     <>
       <div className='App'>
         <Navigation />
-        <SideNav list={list} setList={setList} />
+        <div className='main'>
+          <SideNav list={list} setList={setList} />
 
-        <Switch>
-          <Route path='/recipe/:title?'>
-            <Recipe list={list} />
-          </Route>
-          <Route path='/edit/:title'>
-            <Edit list={list} changeArticle={changeArticle} />
-          </Route>
-          <Route exact path='/'>
-            <Home list={list} setList={setList} />
-          </Route>
-        </Switch>
+          <div className='content'>
+            <Switch>
+              <Route path='/recipe/:title?'>
+                <Recipe list={list} />
+              </Route>
+              <Route path='/edit/:title'>
+                <Edit list={list} changeArticle={changeArticle} />
+              </Route>
+              <Route exact path='/'>
+                <Home list={list} setList={setList} />
+              </Route>
+            </Switch>
+          </div>
+        </div>
       </div>
     </>
   );
